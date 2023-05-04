25f3ed90ee399a91ddb47fd1305dc064b0d5ce75
19066791722k23 <19066791722k23@bootcamp.wethinkcode.co.za>
2023-05-04 22:37:49 +0200

    working on sorting tracks in the playlist

--- src/components/UpdatePlaylist.js
+++ src/components/UpdatePlaylist.js
@@ -16,58 +16,69 @@ const UpdatePlaylist = ({ playlistId, closeUpdate }) => {
     }
   };
 
+  const handleDragEnd = (node, trackId, trackPosition) => {
+    console.log(node);
+  };
+
   useEffect(() => {
     fetchPlaylistTracks();
   }, [playlistId]);
 
   return (
     playlist && (
-      <div className="bg-white dark:bg-background w-[100vw] md:w-[25vw] md:fixed fixed top-0 md:right-0 md:left-auto left-0 shadow border-gray-300 h-[100vh] border-l dark:border-gray-800 px-4 py-5 flex   flex-col pb-44 z-30 text-gray-900 dark:text-white overflow-y-scroll">
+      <div className="bg-white dark:bg-background w-[100vw] md:w-[30vw] md:fixed fixed top-0 md:right-0 md:left-auto left-0 shadow border-gray-300 h-[100vh] border-l dark:border-gray-800 px-4 py-5 flex   flex-col pb-20 z-30 text-gray-900 dark:text-white overflow-y-scroll">
         <button
           onClick={closeUpdate}
           className="w-20 bg-red-700 text-white text-xs px-3 py-1 my-1"
         >
           Close
         </button>
-        <Image
-          src={playlist.images[0]?.url}
-          width={"350"}
-          height={"350"}
-          alt="Playlist image"
-        />
+        {/* {playlist.images[0]?.url && (
+          <Image
+            src={playlist.images[0]?.url}
+            width={"350"}
+            height={"350"}
+            alt="Playlist image"
+          />
+        )} */}
         <h3 className="font-bold text-xl mt-3 dark:text-white text-gray-900">
           {playlist.name}
         </h3>
-        <small className="mb-3">{playlist?.tracks?.items?.length} tracks</small>
-
-        {playlist?.tracks?.items?.map((item, index) => {
-          const track = item.track;
-          return (
-            <div
-              key={track.id}
-              className="flex flex-row items-center gap-3 my-1 px-1"
-            >
-              <p className="w-4">{++index}.</p>
-              <Image
-                src={track.album.images[0].url}
-                width={50}
-                height={50}
-                alt="Cover art"
-              />
-              <h4 className="flex flex-col w-[100%] text-gray-900 dark:text-gray-100 font-medium ">
-                <span className="text-xs md:text-sm ">
-                  {track.name.slice(0, 20) + "..."}
-                </span>
-                <span className="text-xs">
-                  {track.artists
-                    .map((artist) => artist.name)
-                    .join(", ")
-                    .slice(0, 50)}
-                </span>
-              </h4>
-            </div>
-          );
-        })}
+        <p className="mb-3">{playlist?.tracks?.items?.length} tracks</p>
+        <small className=" text-xs">Drag and drop to sort your playlist</small>
+        <div>
+          {playlist?.tracks?.items?.map((item, index) => {
+            const track = item.track;
+            return (
+              <div
+                key={track.id}
+                draggable="true"
+                onDragStart={(e) => console.log("I am getting dragged")}
+                onDragEnd={(e) => handleDragEnd(e, track.id, index)}
+                className="flex flex-row items-center gap-3 my-2 px-1"
+              >
+                <p className="w-4">{++index}.</p>
+                <Image
+                  src={track.album.images[0].url}
+                  width={50}
+                  height={50}
+                  alt="Cover art"
+                />
+                <h4 className="flex flex-col w-[100%] text-gray-900 dark:text-gray-100 font-medium ">
+                  <span className="text-xs md:text-sm ">
+                    {track.name.slice(0, 20) + "..."}
+                  </span>
+                  <span className="text-xs">
+                    {track.artists
+                      .map((artist) => artist.name)
+                      .join(", ")
+                      .slice(0, 50)}
+                  </span>
+                </h4>
+              </div>
+            );
+          })}
+        </div>
       </div>
     )
   );
