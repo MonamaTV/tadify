66f0ad2b41fe265905815359e2de270599354d41
Tadima <19066791722k23@bootcamp.wethinkcode.co.za>
2023-05-27 11:59:58 +0200

    Added the toggle mode on profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,12 +1,25 @@
 import Image from "next/image";
 import Meta from "../../src/components/Head";
+import { useTheme } from "next-themes";
 
 const Profile = () => {
+  const { theme, setTheme } = useTheme();
   return (
     <div className="container">
       <Meta />
-      <div className=" flex relative bg-gradient-to-b from-primary dark:to-background text-gray-900 dark:text-white md:p-10 flex-col md:flex-row sm:flex-row">
-        <div className="md:py-7 md:px-0 px-5 py-5 flex md:flex-row  items-center gap-3">
+
+      <div className=" flex relative bg-gradient-to-b from-primary dark:to-background text-gray-900 dark:text-white md:px-10 md:py-10 flex-col md:flex-row sm:flex-row">
+        <button
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          className="absolute right-1 md:right-10 top-4 md:top-8  rounded-lg text-gray-100 text-xs px-4 py-1 md:flex items-center justify-center "
+        >
+          {theme === "light" ? (
+            <Image src={"/sun.png"} width={20} height={20} alt="user theme" />
+          ) : (
+            <Image src={"/moon.png"} width={20} height={20} alt="user theme" />
+          )}
+        </button>
+        <div className="md:py-1 md:px-0 px-5 py-5 flex md:flex-row  items-center gap-3">
           <Image src="/all.webp" width={"140"} height={"140"} />
           <div className="">
             <h3 className="md:text-5xl font-bold text-3xl select-none">
@@ -25,7 +38,10 @@ const Profile = () => {
         {/* Form */}
         <form className="container mx-auto">
           <div className="flex flex-col my-2">
-            <label className="text-sm my-1" htmlFor="name">
+            <label
+              className="text-gray-900 dark:text-white text-sm my-1 "
+              htmlFor="name"
+            >
               Username:
               <span className="block text-xs text-red-300">
                 *People will be find your profile through this
@@ -35,37 +51,46 @@ const Profile = () => {
             <input
               type="text"
               name="name"
-              className="shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background"
+              className="dark:shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background"
               placeholder="stormbreaker"
             />
           </div>
           <div className="flex flex-col my-2">
-            <label className="text-sm my-1" htmlFor="name">
+            <label
+              className="text-gray-900 dark:text-white text-sm my-1"
+              htmlFor="name"
+            >
               Bio:
             </label>
             <textarea
-              className="shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background resize-none"
+              className="dark:shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background resize-none"
               rows={5}
             ></textarea>
           </div>
           <div className="flex flex-col my-2">
-            <label className="text-sm my-1" htmlFor="name">
+            <label
+              className="text-gray-900 dark:text-white text-sm my-1"
+              htmlFor="name"
+            >
               Choose playlist for your profile:
               <span className="block text-xs text-red-300">
                 *People will be able this playlist
               </span>
             </label>
-            <select className="shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background">
+            <select className="dark:shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background">
               <option>RnB official</option>
               <option>Pop music</option>
               <option>For Lovers</option>
             </select>
           </div>
           <div className="flex flex-col my-2">
-            <label className="text-sm my-1" htmlFor="name">
+            <label
+              className="text-gray-900 dark:text-white text-sm my-1"
+              htmlFor="name"
+            >
               Choose stats for your profile:
             </label>
-            <select className="shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background">
+            <select className="dark:shadow-md px-3 dark:text-white text-gray-900 font-normal outline-none py-3 border  border-gray-900 dark:border-white text-xs w-full dark:bg-background">
               <option>RnB official</option>
               <option>Pop music</option>
               <option>For Lovers</option>
